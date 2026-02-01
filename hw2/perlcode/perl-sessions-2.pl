a9a18ce1b61146510b414215730f99b907a9eea2
LiamMohler <liammohlerr@gmail.com>
2026-02-01 12:29:55 -0800

    perl cgi fortm

--- hw2/perlcode/perl-sessions-2.pl
+++ hw2/perlcode/perl-sessions-2.pl
@@ -34,6 +34,7 @@ if ($name){
 }
 print "<br/><br/>";
 print "<a href=\"perl-sessions-1.pl\">Session Page 1</a><br/>";
+print "<a href=\"perl-cgiform.html\">Perl CGI Form</a><br/>";
 print "<form style=\"margin-top:30px\" action=\"perl-destroy-session.pl\" method=\"get\">";
 print "<button type=\"submit\">Destroy Session</button>";
 print "</form>";
